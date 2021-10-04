78dbda71d8e9f6c473e2c3bfb1717f182e887ee3
Luis Andres Contreras <tremenditomen@gmail.com>
2021-10-03 19:28:21 -0600

    simple timer

---
+++ timer1.js
@@ -0,0 +1,16 @@
+let seconds = process.argv.slice(2);
+
+
+for(let i = 0; i < seconds.length; i++) {
+  const timer = seconds[i] * 1000;
+  
+    if(timer > 0) {
+      setTimeout(() => {
+        process.stdout.write('\x07');
+        
+      }, timer)
+
+    }
+    
+
+  }
