fa8a699040b2f8d67ed41f62fe9350695186992b
Luis Andres Contreras <tremenditomen@gmail.com>
2021-11-02 21:20:25 -0600

    timer1

--- timer1.js
+++ timer1.js
@@ -1,16 +1,11 @@
 let seconds = process.argv.slice(2);
 
-
-for(let i = 0; i < seconds.length; i++) {
+for (let i = 0; i < seconds.length; i++) {
   const timer = seconds[i] * 1000;
-  
-    if(timer > 0) {
-      setTimeout(() => {
-        process.stdout.write('\x07');
-        
-      }, timer)
 
-    }
-    
-
+  if (timer > 0) {
+    setTimeout(() => {
+      process.stdout.write("\x07");
+    }, timer);
   }
+}
